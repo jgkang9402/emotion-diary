31b8a8f363a0b6dd979b4d1d4020566a127ca3a0
jeonggu Kang <93317914+jgkang9402@users.noreply.github.com>
2022-04-22 11:59:20 +0900

    useParams

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,4 +1,10 @@
+import { useParams } from "react-router-dom"; // useParams 리액트에서 만든게아닌 어떤한사람이 편의를위해 만든 훅임. 커스텀훅(사용자정의훅)
+
 const Diary = () => {
+
+  const {id} = useParams();
+  console.log(id)
+
   return (
     <div>
       <h1>Diary</h1>
