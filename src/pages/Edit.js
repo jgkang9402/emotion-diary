8fcb4bcfd826c296ef9f3982324822d0acc83f16
jeonggu Kang <93317914+jgkang9402@users.noreply.github.com>
2022-04-25 12:09:28 +0900

    useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,7 @@
-import {useSearchParams} from "react-router-dom"
+import {useNavigate, useSearchParams} from "react-router-dom"
 
 const Edit = () => {
-
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams()
 
   const id = searchParams.get("id")
@@ -15,6 +15,12 @@ const Edit = () => {
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
       <button onClick={()=>setSearchParams({who:'johnny'})}>QS 바꾸기</button>
+      <button onClick={()=>{
+        navigate('/home')
+      }}>Home으로 가기</button>
+      <button onClick={()=>{
+        navigate(-1)
+      }}>뒤로가기</button>
     </div>
   );
 };
